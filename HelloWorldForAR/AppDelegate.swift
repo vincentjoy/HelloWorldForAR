f8af0eef5f57fa631a25211c7e6d2209180e75eb
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-05 14:19:56 +0530

    added a 3D sphere to the scene

--- HelloWorldForAR/AppDelegate.swift
+++ HelloWorldForAR/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    // Test commit
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- HelloWorldForAR/ViewController.swift
+++ HelloWorldForAR/ViewController.swift
@@ -8,67 +8,26 @@
 import UIKit
 import SceneKit
 import ARKit
+import RealityKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        sceneView.delegate = self
+        // 1. 3D Model
+        let sphere = MeshResource.generateSphere(radius: 0.05)
+        let material = SimpleMaterial(color: .red, roughness: 0, isMetallic: true)
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let sphereEntity = ModelEntity(mesh: sphere, materials: [material])
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // 2. Create anchor
+        let sphereAnchor = AnchorEntity(world: SIMD3(x: 0, y: 0, z: 0))
+        sphereAnchor.addChild(sphereEntity)
         
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        // 3. Add anchor to scene
+        arView.scene.addAnchor(sphereAnchor)
     }
 }
