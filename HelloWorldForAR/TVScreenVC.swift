c1e76580dca307c45bb6c263bdc6e8ecbf98116d
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-06 14:03:59 +0530

    added the plane detection and tap detection to place the video screen on the plane

--- HelloWorldForAR/TVScreenVC.swift
+++ HelloWorldForAR/TVScreenVC.swift
@@ -3,6 +3,7 @@
 import UIKit
 import RealityKit
 import AVKit
+import ARKit
 
 class TVScreenVC: UIViewController {
     
@@ -10,6 +11,62 @@ class TVScreenVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Start off plane detection
+        startPlaneDetection()
+        
+        startTapDetection()
+    }
+    
+    func startPlaneDetection() {
+        arView.automaticallyConfigureSession = true
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal]
+        configuration.environmentTexturing = .automatic
+        
+        arView.session.run(configuration)
+    }
+    
+    func startTapDetection() {
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recogonizer:))))
+    }
+    
+    @objc func handleTap(recogonizer: UITapGestureRecognizer) {
+        let tapLocation = recogonizer.location(in: arView)
+        
+        let results = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        if let firstResult = results.first {
+            
+            // 3D point
+            let worldPosition = simd_make_float3(firstResult.worldTransform.columns.3)
+            
+            // Place screen at 3D point
+            if let videoScreen = createVideoScreen(width: 0.4, height: 0.2) {
+                videoScreen.setPosition(SIMD3(x:0, y:0.2/2, z:0), relativeTo: videoScreen) // y position is like that, to lift the video screen little above the plane, so no part of it will be going under the table
+                placeScreen(videoScreen, at: worldPosition)
+                
+                // Enable gestures
+                installGestures(on: videoScreen)
+            }
+        }
+    }
+    
+    func placeScreen(_ screen: ModelEntity, at worldPosition: SIMD3<Float>) {
+        
+        // Anchor
+        let anchor = AnchorEntity(world: worldPosition)
+        
+        // Tie model to anchor
+        anchor.addChild(screen)
+        
+        // Anchor to scene
+        arView.scene.addAnchor(anchor)
+    }
+    
+    func installGestures(on model: ModelEntity) {
+        model.generateCollisionShapes(recursive: true)
+        arView.installGestures([.rotation, .scale], for: model)
     }
     
     // MARK:- Video Screen
